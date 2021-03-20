dcc4a89a6359e25b28eda651204731d9cc82d8b1
Arvin Fernandez <arvinf07@gmail.com>
2021-03-20 08:59:57 -0400

    commit tree 4d3d08641dbbfe0b92d70839246f4ca0b12fee00

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,65 @@
-// Code MovieReviews Here
+// import React from 'react'
+
+// export default function MovieReviews(props){
+
+//   const renderReviews = (reviews) => {
+//     return reviews.map( (reviewObj) => {
+//       return(
+//       <div className='reviews'>
+//         <h3>{reviewObj.display_title}</h3>
+//         Summary: {reviewObj.summary_short}
+//       </div>
+//       )
+//     })
+//   }
+
+//   return(
+//     <div className='review-list'>
+//       {renderReviews(props.reviews)}
+//     </div>
+//   )
+// }
+
+// MovieReviews.defaultProps = {
+//   reviews: []
+// };
+
+
+
+
+import React from 'react';
+
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
+
+    <div
+      key={headline}
+      className="review"
+    >
+      <header>
+        <a
+          className="review-link"
+          href={link.url}
+        >
+          {headline}
+        </a>
+        <br/>
+        <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  );
+};
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
